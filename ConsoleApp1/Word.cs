7c62685582b61f066efe8afc68a9040df7d70383
Jiyun.Jung <modogo3@gmail.com>
2025-08-19 17:31:09 +0900

    커밋

--- ConsoleApp1/Word.cs
+++ ConsoleApp1/Word.cs
@@ -3,23 +3,27 @@ namespace ConsoleApp1;
 
 public class Word
 {
-    private string word = " ";
+    private string word = "retry";
 
     public bool IsVowel(int i)
     {
         //word[i]가 'a'와 같은지 확인
-        if (i < 0 || i >= _word.Length) throw new System.ArgumentOutOfRangeException(nameof(i));
-        char c = char.ToLowerInvariant(_word[i]);
-        return c is 'a' or 'e' or 'i' or 'o' or 'u';
+        if (i < 0 || i >= word.Length)
+        {
+            return false;
+        }
+        char c=char.ToLower(word[i]);
+        string vowel = "aeiou";
+        return vowel.Contains(c);
     }
     
     //i번째 글자가 자음인지 알려주는 IsConsonant() 함수 수정
 
     public bool IsConsonant(int i)
     {
-        if(i<0||i>=_word.Length) throw new System.ArgumentOutOfRangeException(nameof(i));
-        char ch = _word[i];
-        if (!char.IsLetter(ch)) return false;
+        if(i<0||i>=word.Length) throw new System.ArgumentOutOfRangeException(nameof(i));
+        char c = word[i];
+        if (!char.IsLetter(c)) return false;
         return !IsVowel(i);
     }
 }
