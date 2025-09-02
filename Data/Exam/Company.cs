5f37ea81361547f1bce2d8d2a84c4405f5babc9f
Jiyun.Jung <modogo3@gmail.com>
2025-09-02 17:36:40 +0900

    문제 풀이

---
+++ Data/Exam/Company.cs
@@ -0,0 +1,46 @@
+using Newtonsoft.Json;
+using System.IO;
+using System;
+
+namespace Data;
+
+/*총무부 리더 홍길동(41세) 인스턴스를 직렬화하여 company.json 파일에 Json String 형태로 저장하는
+프로그램을 작성하시오*/
+
+public class Company
+{
+    static void Main(string[] args)
+    {
+        Company.Employee employees = new Company.Employee("홍길동", 41);
+        Company.Department department = new
+            Company.Department("홍길동", employee);
+
+        string jsonString = JsonConvert.SerializeObject(department);
+        File.WriteAllText("department.json", jsonString);
+    }
+    
+    public class Employee
+
+    {
+        public string Name { get; }
+        public int Age { get; }
+
+        public Employee(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    
+    class Department
+    {
+        public string Name { get; }
+        public Employee leader { get; }
+
+        public Department(string name, Employee leader)
+        {
+            Name = name;
+            this.leader = leader;
+        }
+    }
+}
