607614abc46d839421f6959711673cea6b6acb2f
Jiyun.Jung <modogo3@gmail.com>
2025-09-02 16:55:48 +0900

    json 생성

--- Data/Exam/Employee.cs
+++ Data/Exam/Employee.cs
@@ -1,6 +1,8 @@
 namespace Data;
 
 public class Employee
+/*총무부 리더 홍길동(41세) 인스턴스를 직렬화하여 company.json 파일에 Json String 형태로 저장하는
+프로그램을 작성하시오*/
 {
     public string Name { get; }
     public int Age { get; }
