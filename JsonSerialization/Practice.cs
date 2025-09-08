87a63aa7450f200e5551b086276ab8203de5600a
Jiyun.Jung <modogo3@gmail.com>
2025-09-08 17:01:30 +0900

    main 추가

--- JsonSerialization/Practice.cs
+++ JsonSerialization/Practice.cs
@@ -1,5 +1,3 @@
-namespace JsonSerialization;
-
 //한 새는 1초마다, 다른 새는 2초마다, 마지막 새는 3초마다 소리를 냅니다
 /*요구사항
  각 새의 소리 타이밍을 재현하되, 각 새마다 하나의 비동기 함수를 사용하세요
@@ -8,8 +6,22 @@ namespace JsonSerialization;
  두 번째 새는 "까악" 소리를 냅니다
  마지막 새는 "짹짹" 소리를 냅니다
  모든 새소리가 끝나면 프로그램이 종료되어야 합니다*/
+
+namespace JsonSerialization;
 public class Practice
 {
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("--- 실행 시작 ---");
+        
+        Task task1 = Practice.First();
+        Task task2 = Practice.Second();
+        Task task3 = Practice.Last();
+        
+        await Task.WhenAll(task1, task2, task3);
+    
+        Console.WriteLine("--- 실행 종료 ---");
+    }
     public static async Task First()
     {
         for (int i = 0; i < 4; i++)
--- JsonSerialization/Program.cs
+++ JsonSerialization/Program.cs
@@ -15,6 +15,7 @@ class Program
             this.Age = age;
         }
     }
+
     public class Department
     {
         public string Name { get; }
@@ -26,31 +27,32 @@ class Program
             this.leader = leader;
         }
     }
-
-    static void Main(string[] args)
-    {
-        Department department = new Department(
-            name: "총무부",
-            leader: new Employee(name: "홍길동", age: 41));
-        
-        string jsonString=JsonConvert.SerializeObject(department);
-        
-        File.WriteAllTextAsync("department.json", jsonString).ContinueWith(Task =>
-        {
-            File.WriteAllTextAsync("hero.txt", "영웅 정보").ContinueWith(task =>
-            {
-                File.WriteAllTextAsync("hero2.txt", "영웅2 정보").ContinueWith(task => { Console.WriteLine("최종 완료"); });
-            });
-            Console.WriteLine("저장 완료 : 2");
-        });
-        Task.Delay(1000).ContinueWith(task =>
-        {
-            File.WriteAllTextAsync("hero3.txt", "영웅3 정보").ContinueWith(task1 => { Console.WriteLine("hero3 완료"); });
-        });
-        Console.WriteLine("저장 완료 : 1");
-
-        File.WriteAllTextAsync("hero4.txt", "영웅4 정보").ContinueWith(task => { Console.WriteLine("hero4 완료"); });
-
-        Console.ReadLine();
-    }
 }
+
+/*static void Main(string[] args)
+{
+    Department department = new Department(
+        name: "총무부",
+        leader: new Employee(name: "홍길동", age: 41));
+
+    string jsonString=JsonConvert.SerializeObject(department);
+
+    File.WriteAllTextAsync("department.json", jsonString).ContinueWith(Task =>
+    {
+        File.WriteAllTextAsync("hero.txt", "영웅 정보").ContinueWith(task =>
+        {
+            File.WriteAllTextAsync("hero2.txt", "영웅2 정보").ContinueWith(task => { Console.WriteLine("최종 완료"); });
+        });
+        Console.WriteLine("저장 완료 : 2");
+    });
+    Task.Delay(1000).ContinueWith(task =>
+    {
+        File.WriteAllTextAsync("hero3.txt", "영웅3 정보").ContinueWith(task1 => { Console.WriteLine("hero3 완료"); });
+    });
+    Console.WriteLine("저장 완료 : 1");
+
+    File.WriteAllTextAsync("hero4.txt", "영웅4 정보").ContinueWith(task => { Console.WriteLine("hero4 완료"); });
+
+    Console.ReadLine();
+}
+}*/
