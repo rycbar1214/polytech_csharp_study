1adc2de847d47036edf54f3ccaa036c1e641fbfe
Jiyun.Jung <modogo3@gmail.com>
2025-09-08 14:09:09 +0900

    비동기 프로그래밍

---
+++ Data/Program.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+namespace Data;
+
+class Sword
+{
+    public string Name { get; set; }
+
+    public Sword(string name)
+    {
+        Name = name;
+    }
+}
+
+class Hero
+{
+    public string Name { get; set; }
+    public int Hp { get; set; }
+    public int Mp { get; set; }
+    public List<string> Items { get; set; }=new List<string>();
+    public Sword? Sword { get; set; }
+
+    public Hero(string name, int hp, int mp)
+    {
+        Name = name;
+        Hp = hp;
+        Mp = mp;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Hp)}: {Hp}, {nameof(Mp)}: {Mp}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        JsonSerializtion();
+        JsonDeserialztion();
+    }
+
+    static void JsonSerializtion()
+    {
+        Hero hero = new Hero("홍길동", 100, 50);
+        hero.Items.Add("물약");
+        hero.Items.Add("반창고");
+        hero.Sword = new Sword("불의 검");
+        
+        string jsonString = JsonConvert.SerializeObject(hero);
+        Console.WriteLine(hero);
+        Console.WriteLine(jsonString);
+        File.WriteAllText("hero.json", jsonString);
+    }
+
+    static void JsonDeserialztion()
+    {
+        string jsonString=File.ReadAllText("hero.json");
+        Hero hero=JsonConvert.DeserializeObject<Hero>(jsonString);
+        Console.WriteLine(hero);
+    }
+
+    static void PropertyParser()
+    {
+        Dictionary<string, object> heroes = new Dictionary<string, object>();
+        
+        string[] heroesArray=File.ReadAllLines("hero.properties");
+
+        foreach (var hero in heroesArray)
+        {
+            string key = hero.Split("=")[0];
+            string value = hero.Split("=")[1];
+            heroes.Add(key, value);
+        }
+
+        foreach (var keyValuePair in heroes)
+        {
+            Console.WriteLine(keyValuePair.Key+" : "+keyValuePair.Value);
+        }
+    }
+
+    static void CsvParser()
+    {
+        List<Hero> heroes=File.ReadAllLines("hero.csv")
+            .Skip(1)
+            .Select(line =>
+            {
+                string name = line.Split("=")[0];
+                int hp = int.Parse(line.Split(",")[1]);
+                int mp = int.Parse(line.Split(",")[2]);
+                return new Hero(name, hp, mp);
+            })
+            .ToList();
+        
+        heroes.ForEach(Console.WriteLine);
+    }
+}
---
+++ JsonSerialization/Program.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonSerialization;
+
+class Program
+{
+    public class Employee
+    {
+        private string Name { get; }
+        private int Age { get; }
+
+        public Employee(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+    public class Department
+    {
+        public string Name { get; }
+        public Employee leader { get; }
+
+        public Department(string name, Employee leader)
+        {
+            this.Name = name;
+            this.leader = leader;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Department department = new Department(
+            name: "총무부",
+            leader: new Employee(name: "홍길동", age: 41));
+        
+        string jsonString=JsonConvert.SerializeObject(department);
+        
+        File.WriteAllTextAsync("department.json", jsonString).ContinueWith(Task =>
+        {
+            File.WriteAllTextAsync("hero.txt", "영웅 정보").ContinueWith(task =>
+            {
+                File.WriteAllTextAsync("hero2.txt", "영웅2 정보").ContinueWith(task => { Console.WriteLine("최종 완료"); });
+            });
+            Console.WriteLine("저장 완료 : 2");
+        });
+        Task.Delay(1000).ContinueWith(task =>
+        {
+            File.WriteAllTextAsync("hero3.txt", "영웅3 정보").ContinueWith(task1 => { Console.WriteLine("hero3 완료"); });
+        });
+        Console.WriteLine("저장 완료 : 1");
+
+        File.WriteAllTextAsync("hero4.txt", "영웅4 정보").ContinueWith(task => { Console.WriteLine("hero4 완료"); });
+
+        Console.ReadLine();
+    }
+}
