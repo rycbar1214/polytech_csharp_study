bc846c2337330a0aca83fcc9939925cd81073aca
Jiyun.Jung <modogo3@gmail.com>
2025-09-12 15:38:40 +0900

    커밋

---
+++ CsharpStudy.Http/DataSources/IDataSource.cs
@@ -0,0 +1,12 @@
+using CsharpStudy.Http.DataSources;
+
+namespace CsharpStudy.Http.DataSources;
+
+public interface IDataSource<T>
+{
+    Task<Response<List<T>>> GetAllAsync();
+    Task<Response<T>> GetByIdAsync(int id);
+    Task<Response<T>> CreateAsync(T item);
+    Task<Response<T>> UpdateAsync(T item);
+    Task<Response<bool>> DeleteAsync(int id);
+}
